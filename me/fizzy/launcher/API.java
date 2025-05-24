b273c541b87a17ae881fa14cdf41e3babd40ac6d
Kolja <fabianrose339@gmail.com>
2025-05-24 14:18:48 +0200

    Add files via upload

---
+++ me/fizzy/launcher/API.java
@@ -0,0 +1,11 @@
+package me.fizzy.launcher;
+
+public class API {
+    public static String getName() {
+        return "LauncherUser";
+    }
+
+    public static int getRank() {
+        return 10;
+    }
+}
